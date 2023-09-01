ab49bee583a02608e10635e343ebd1aa0d274834
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-09-01 16:41:31 +0200

    Finish commit
    Finish ex

--- js/myscript.js
+++ js/myscript.js
@@ -7,51 +7,51 @@ Dichiariamo chi ha vinto.*/
 
 
 
-// let choiceEvenOrOdd = prompt('scegli pari o dispari').toLowerCase()
-// choiceEvenOrOdd = document.getElementById("choiceUser").innerHTML = choiceEvenOrOdd
+let choiceEvenOrOdd = prompt('scegli pari o dispari').toLowerCase()
+choiceEvenOrOdd = document.getElementById("choiceUser").innerHTML = choiceEvenOrOdd
 
 
-// let choiceUserNumber = Number(prompt('inserisci un numero da 1 a 5'))
-// choiceUserNumber = document.getElementById("choiceNumber").innerHTML = choiceUserNumber
+let choiceUserNumber = Number(prompt('inserisci un numero da 1 a 5'))
+choiceUserNumber = document.getElementById("choiceNumber").innerHTML = choiceUserNumber
 
 
-// let pcNumber = generatoreNumeri();
-// pcNumber = document.getElementById("generator").innerHTML = generatoreNumeri()
-// // console.log('numero pc ' + pcNumber);
+let pcNumber = generatoreNumeri();
+pcNumber = document.getElementById("generator").innerHTML = generatoreNumeri()
+// console.log('numero pc ' + pcNumber);
 
-// function generatoreNumeri() {
-//     return Math.floor(Math.random() * 5) + 1 ; 
-// }
+function generatoreNumeri() {
+    return Math.floor(Math.random() * 5) + 1 ; 
+}
 
 
-// function sommaNumeri(num1,num2) {
-//     const somma = num1 + num2 ; 
-//     return somma;           
-// }
+function sommaNumeri(num1,num2) {
+    const somma = num1 + num2 ; 
+    return somma;           
+}
 
-// let somma = sommaNumeri(pcNumber, choiceUserNumber)
-// somma = document.getElementById("add").innerHTML = somma
-// // console.log('La somma è ' + somma);
+let somma = sommaNumeri(pcNumber, choiceUserNumber)
+somma = document.getElementById("add").innerHTML = somma
+// console.log('La somma è ' + somma);
 
-// function pari_dispari(somma) {
-//     if (somma % 2 === 0){
-//         return "pari";
-//     }else{
-//         return "dispari";
+function pari_dispari(somma) {
+    if (somma % 2 === 0){
+        return "pari";
+    }else{
+        return "dispari";
 
-//     } 
+    } 
 
-// }
+}
 
-// let evenorodd = document.getElementById("pariDispari").innerHTML = pari_dispari(somma)
-// // console.log(pari_dispari(somma));
+let evenorodd = document.getElementById("pariDispari").innerHTML = pari_dispari(somma)
+// console.log(pari_dispari(somma));
 
-// if(pari_dispari === choiceEvenOrOdd){
-//     let resultGame = document.getElementById("resultChoice").innerHTML = 'You Win'   
-// }else {
-//     let resultGame = document.getElementById("resultChoice").innerHTML = 'You Lose'
+if(pari_dispari === choiceEvenOrOdd){
+    let resultGame = document.getElementById("resultChoice").innerHTML = 'You Win'   
+}else {
+    let resultGame = document.getElementById("resultChoice").innerHTML = 'You Lose'
 
-// }
+}
 
 
 
@@ -60,34 +60,66 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
  */
 
-let userWord = prompt("Inserisci una parola")
+// let userWord = prompt("Inserisci una parola")
 
-let arraysplit = userWord.split('');
-console.log(arraysplit);
+// let arraysplit = userWord.split('');
+// console.log(arraysplit);
 
-let arrayreverse = arraysplit.reverse();
-console.log(arrayreverse);
+// let arrayreverse = arraysplit.reverse();
+// console.log(arrayreverse);
 
-let invertArray = arraysplit.join('');
-console.log(invertArray);
-
-
-function palindroma(word1, word2) {
-    const check = word1 == word2
+// let invertArray = arraysplit.join('');
+// console.log(invertArray);
 
 
-    if (userWord == invertArray) {
-        let verified = document.getElementById("yesOrNot").innerHTML = ("la parola è palindroma");
-        return "palindroma"
+// function palindroma(word1, word2) {
+//     const check = word1 == word2
+
+
+//     if (userWord == invertArray) {
+//         let verified = document.getElementById("yesOrNot").innerHTML = ("la parola è palindroma");
+//         return "palindroma"
         
-    } else (userWord != invertArray)
-    {
-        let verified = document.getElementById("yesOrNot").innerHTML = ("la parola non è palindroma");
-       return "non è palindroma"
+//     } else (userWord != invertArray)
+//     {
+//         let verified = document.getElementById("yesOrNot").innerHTML = ("la parola non è palindroma");
+//        return "non è palindroma"
        
-    } 
+//     } 
     
+// }
+
+// let check = palindroma(userWord, invertArray)
+// console.log(check);
+
+const userWord = document.getElementById('userWord');
+
+
+function isPalindrome(word) {
+    let wordReverse = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        wordReverse += word[i];
+    console.log(wordReverse);
+    }
+   
+    if (wordReverse === word) {
+        return true
+    }
+
+    return false
 }
 
-let check = palindroma(userWord, invertArray)
-console.log(check);
+//isPalindrome('cacca');
+//console.log(isPalindrome('otto'));
+
+const paliButton = document.getElementById('palindrome');
+const paliResultDom = document.getElementById('palindromeResult');
+
+paliButton.addEventListener('click', function () {
+
+    if (isPalindrome(userWord.value)) {
+        paliResultDom.innerHTML = "La parola è palindroma";
+    } else {
+        paliResultDom.innerHTML = "La parola non è palindroma";
+    }
+});
